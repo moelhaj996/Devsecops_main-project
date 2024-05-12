40fe3abc62e8666c8b5d99f685e3c93c5315c8ce
moelhaj996 <mohsuliman.996@gmail.com>
2024-05-12 16:23:26 +0200

    update

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "eu-west-2" # London region
+}
+resource "aws_s3_bucket" "terraform-march-mohamed" {
+  bucket = "terraform-march-mohamed"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-march-mohamed"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
+resource "aws_instance" "test5" {
+  ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
+  instance_type = "t2.micro"  # Change to the desired instance type
+  tags = {
+    Name = "sundayexample-instance"
+}
+}
